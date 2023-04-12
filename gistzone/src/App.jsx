e0ba5fcd6bf814692bba722ca92221a808517501
Niceiyke <oyomworld@gmail.com>
2023-04-12 20:56:35 +0100

    update

--- gistzone/src/App.jsx
+++ gistzone/src/App.jsx
@@ -1,18 +1,24 @@
+import { Routes,Route} from "react-router-dom"
 import FooterLayout from "./Layout/Footer"
 import Layout from "./Layout/Layout"
 import Header from "./components/Header"
-
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
 
 
   return (
     <div className="container mx-auto">
-      <Header/>
-      <Layout/>
-      <FooterLayout/>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path ='/' element={<Layout/>}/>
+      
+
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
--- gistzone/src/Layout/Layout.jsx
+++ gistzone/src/Layout/Layout.jsx
@@ -2,23 +2,30 @@ import { Footer } from "flowbite-react";
 import LeftSideBar from "./LeftSideBar";
 import MainFeed from "./MainFeed";
 import RightSideBar from "./RightSideBar";
+import Header from "../components/Header";
+import FooterLayout from "./Footer";
 
 
 function Layout() {
   return (
-    <div className="min-h-screen grid grid-cols-1 md:grid-cols-12 md:gap-2">
-      <div className="p-4 bg-secondary md:col-span-4 md:order-first">
-        <LeftSideBar />
+    <>
+    <Header/>
+      <div className="min-h-screen grid grid-cols-1 md:grid-cols-12 md:gap-2">
+        <div className="p-4 bg-secondary md:col-span-4 md:order-first">
+          <LeftSideBar />
+        </div>
+
+        <div className="order-first p-4 md:col-span-6">
+          <MainFeed />
+        </div>
+
+        <div className="p-4 bg-secondary md:col-span-2 md:order-last">
+          <RightSideBar />
+        </div>
       </div>
 
-      <div className="order-first p-4 md:col-span-6">
-        <MainFeed />
-      </div>
-
-      <div className="p-4 bg-secondary md:col-span-2 md:order-last">
-        <RightSideBar />
-      </div>
-    </div>
+      <FooterLayout/>
+    </>
   );
 }
 
